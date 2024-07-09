5547b2b37bf1fe1c7769eeab53e66b20f815845b
Dilexi <dilexilex97@gmail.com>
2024-07-09 13:42:51 +0300

    подготовил парсер к сетевым сбоям

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,4 +1,5 @@
 import requests
+from time import sleep
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
@@ -6,14 +7,20 @@ from bs4 import BeautifulSoup
 def get_category_book_url(start_page, end_page):
     all_full_urls = []
     for number_page in range(start_page, end_page):
-        tululu_url = f'https://tululu.org/l55/{number_page}'
-        response = requests.get(tululu_url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'lxml')
-        books_selector = 'table.d_book'
-        books_urls = soup.select(books_selector)
-        for book_url in books_urls:
-            url = book_url.find('a')['href']
-            book_full_url = urljoin(tululu_url, url)
-            all_full_urls.append(book_full_url)
+        try:
+            tululu_url = f'https://tululu.org/l55/{number_page}'
+            response = requests.get(tululu_url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'lxml')
+            books_selector = 'table.d_book'
+            books_urls = soup.select(books_selector)
+            for book_url in books_urls:
+                url = book_url.find('a')['href']
+                book_full_url = urljoin(tululu_url, url)
+                all_full_urls.append(book_full_url)
+        except requests.exceptions.HTTPError:
+            print('книга не найдена')
+        except requests.exceptions.ConnectionError:
+            print("Повторное подключение к серверу")
+            sleep(20)
     return all_full_urls
