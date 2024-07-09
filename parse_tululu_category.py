1dd9269c99b771f834f13f71c9be96899f7cd478
Dilexi <dilexilex97@gmail.com>
2024-07-09 13:27:29 +0300

    Поправил множественное число в названиях

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -3,17 +3,17 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 
-def get_category_books_url(start_page, end_page):
-    all_full_url = []
+def get_category_book_url(start_page, end_page):
+    all_full_urls = []
     for number_page in range(start_page, end_page):
         tululu_url = f'https://tululu.org/l55/{number_page}'
         response = requests.get(tululu_url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'lxml')
         books_selector = 'table.d_book'
-        books_url = soup.select(books_selector)
-        for book_url in books_url:
+        books_urls = soup.select(books_selector)
+        for book_url in books_urls:
             url = book_url.find('a')['href']
             book_full_url = urljoin('https://tululu.org', url)
-            all_full_url.append(book_full_url)
-    return all_full_url
+            all_full_urls.append(book_full_url)
+    return all_full_urls
