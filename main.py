84d893a81d89ca9ee86c3ba71b0d4bfbdbafd735
Dilexi <dilexilex97@gmail.com>
2024-07-10 10:28:28 +0300

    Перераспределил функции по файлам

--- main.py
+++ main.py
@@ -8,9 +8,34 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin, unquote, urlsplit
 
-from check_for_redirect import check_for_redirect
-from parse_tululu_category import get_category_book_url
-   
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
+def get_category_book_url(start_page, end_page):
+    all_full_urls = []
+    for number_page in range(start_page, end_page):
+        try:
+            tululu_url = f'https://tululu.org/l55/{number_page}'
+            response = requests.get(tululu_url)
+            response.raise_for_status()
+            check_for_redirect(response)
+            soup = BeautifulSoup(response.text, 'lxml')
+            books_selector = 'table.d_book'
+            books_urls = soup.select(books_selector)
+            for book_url in books_urls:
+                url = book_url.find('a')['href']
+                book_full_url = urljoin(tululu_url, url)
+                all_full_urls.append(book_full_url)
+        except requests.exceptions.HTTPError:
+            print('книга не найдена')
+        except requests.exceptions.ConnectionError:
+            print("Повторное подключение к серверу")
+            sleep(20)
+    return all_full_urls
+
 
 def download_txt(url, number, filename, folder='books/'):
     os.makedirs(folder, exist_ok=True)
